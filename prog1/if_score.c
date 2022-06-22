b163d8783eb9f22339d463008b66b1bbeb1e254f
cnnkj <83521256+cnnkj@users.noreply.github.com>
2022-06-22 10:20:53 +0700

    Create if_score.c

---
+++ prog1/if_score.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+	int n, score, count;
+	count = 0;
+	scanf("%d", &n);
+	for(int i = 0; i < n; i++){
+		scanf("%d", &score);
+		if(){
+			
+		}		
+	}
+	printf("All pass is %d", count);
+	return 0;
+}
